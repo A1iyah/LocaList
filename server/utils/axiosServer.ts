16dc241747457cc5c30e78c9eb48f60392056d28
aliyah <aliyah1mesi@gmail.com>
2024-06-10 20:09:39 +0300

    axios-server

--- server/utils/axiosServer.ts
+++ server/utils/axiosServer.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosServer = axios.create({
-  baseURL: "https://shazam-core.p.rapidapi.com",
+  baseURL: process.env.BASE_URL,
   headers: {
-    "x-rapidapi-key": "696200657cmsh29253769644c40fp10230djsna6a4b5b94706",
-    "x-rapidapi-host": "shazam-core.p.rapidapi.com",
+    "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+    "x-rapidapi-host": process.env.RAPIDAPI_HOST,
   },
 });
